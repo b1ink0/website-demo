97f88e54768c1f118c188b167654b39bb1d5f3b0
stupidgaming <loginfor357@gmail.com>
2021-03-11 00:18:36 +0530

    commit

---
+++ src/components/Navbar/Menu.js
@@ -0,0 +1,17 @@
+export const Menu = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Videos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+import { Menu } from './Menu';
+import X from './img/x.svg'
+import X1 from './img/img/x.svg'
+import E from './img/e.svg'
+import E1 from './img/img/e.svg'
+import XA from './img/xa.svg'
+import XA1 from './img/img/xa.svg'
+import EA from './img/ea.svg'
+import EA1 from './img/img/ea.svg'
+
+class Navbar extends Component {
+    state = { clicked: false, clicked: false, svg: E }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+        console.log(this.state.clicked)
+        if (this.state.clicked){
+            this.setState({ svg: XA})
+        }else {
+           
+            this.setState({ svg: EA })
+        }
+    }
+    render(){
+        return(
+            <div>
+                <nav>
+                    <div className='menu-logo'>
+                        <div className='c'></div>
+                        <div className='d'></div>
+                        <h1>Stup!d Gaming </h1>
+                    </div>
+                    <div className='burger'>
+                        <img onClick={this.handleClick} className='burger-img' src={this.state.svg}></img>
+                    </div>
+                    <div className={this.state.clicked ? 'menu active' : 'menu'}>
+                        <ul>
+                            {
+                                Menu.map((items, index)=>{
+                                    return( 
+                                        <div key={index} className='contain-1' id='contain-1'>
+                                            <div className='a'></div>
+                                            <div className='b'></div>
+                                            <li key={index} className={items.cName}>{items.title}</li>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+};
+export default Navbar;
